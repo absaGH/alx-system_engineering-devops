6241ba8642d10c718a324b933d9c135c60f6d4d1
absaGH <absa3852@gmail.com>
2021-09-28 16:45:18 +0000

    api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Display the top 10 hottest titles in a subreddit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/absaGH)"
     }
     params = {
         "limit": 10
