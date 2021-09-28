254b9d5f133dd6550cb4c9ad29c2cb69ee7efb36
absaGH <absa3852@gmail.com>
2021-09-28 17:22:51 +0000

    advanced api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     """Prints counts of given keywordst case insensitive"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/absaGH)"
     }
     params = {
         "after": after,
