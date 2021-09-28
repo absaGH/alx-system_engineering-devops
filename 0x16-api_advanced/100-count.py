d79d1cc50f8d84284ed67d0b41196277892527fa
absaGH <absa3852@gmail.com>
2021-09-28 17:17:51 +0000

    advanced api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -31,11 +31,11 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         title = c.get("data").get("title").lower().split()
         for word in word_list:
             if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
+                times = len([t for t in title if t.lower() == word.lower()])
+                if instances.get(word.lower()) is None:
+                    instances[word.lower()] = times
                 else:
-                    instances[word] += times
+                    instances[word.lower()] += times
 
     if after is None:
         if len(instances) == 0:
