f0f2ca1965696f00a416357995069cbe95f8965e
absaGH <absa3852@gmail.com>
2021-09-28 16:38:45 +0000

    advanced api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function to query hot posts on a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """Display the top 10 hottest titles in a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+        results = response.json().get("data")
+        [print(t.get("data").get("title")) for t in results.get("children")]
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,8 @@
+# 0x16. API advanced
+The tasks in this project further explore the concept of API. The tasks use the Reddit API.
+The concepts covered in this project are:
+* How to read API documentation to find the endpoints you’re looking for
+* How to use an API with pagination
+* How to parse JSON results from an API
+* How to make a recursive API call
+* How to sort a dictionary by value
