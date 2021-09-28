e61cfb5d32a897277ccd77cb5319f9133c8fd516
absaGH <absa3852@gmail.com>
2021-09-28 16:53:04 +0000

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the subreddit provided as argument and return subscriber No."""
+    """queries the subreddit provided as argument and return subscriber No"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced"
+        "User-Agent": "linux:0x16.api.advanced: :v1.0.0 (by /u/absaGH)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
