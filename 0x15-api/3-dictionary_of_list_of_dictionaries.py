7427845346638d07204a44beb37eba59f02117ce
absaGH <absa3852@gmail.com>
2021-11-08 15:13:13 +0000

    store todo list information of all employees to json file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exports todo list information of all employees to JSON file"""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfl:
+        json.dump({
+            emp.get("id"): [{
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": emp.get("username")
+            } for task in requests.get(url + "todos",
+                                       params={"userId": emp.get("id")}).json()]
+            for emp in users}, jsonfl)
